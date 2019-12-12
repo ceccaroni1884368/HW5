052f9b20f1d77a76255f652c254f2c31267152bf
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-12-12 23:27:29 +0100

    Update func_4.py

--- func_4.py
+++ func_4.py
@@ -3,7 +3,26 @@ from math import sqrt
 
 def Functionality4(node, set_nodes, dist, nodes):
 
+    """
+    :param node: the start node H
+    :type node: int
+    :param set_nodes: set of node to visit
+    :type set_nodes: list
+    :param dist: distance between nodes (d, t, network distance)
+    :type dist: dict
+    :param nodes: nodes of the graph
+    :type nodes: dict
+    :return: the shortest path that visits the set_nodes in a list
+
+    This function visualize the Shortest Approximate Route and
+    return the list contain the shortest path
+    """
+
     def h(nodes, current, neighbor):
+
+        """
+        Euclidean distance between current and neighbor
+        """
         x1 = nodes[current]['Latitude']
         y1 = nodes[current]['Longitude']
         x2 = nodes[neighbor]['Latitude']
@@ -11,24 +30,31 @@ def Functionality4(node, set_nodes, dist, nodes):
         return sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
     def reconstruct_path(cameFrom, current):
+
+        """
+        This function take as input a dict with the connected nodes
+        and orders them in a chaim
+        """
         total_path = [current]
         while current in cameFrom.keys():
             current = cameFrom[current]
             total_path.insert(0, current)
         return total_path
 
-
-    # A* finds a path from start to goal.
-    # h is the heuristic function. h(n) estimates the cost to reach goal from node n.
     def A_Star(nodes, graph, start, goal, h):
+        # A* finds a path from start to goal.
+        # h is the heuristic function. h(n) estimates the cost to reach goal
+        # from node n.
         # The set of discovered nodes that may need to be (re-)expanded.
         # Initially, only the start node is known.
         openSet = {start}
 
-        # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
+        # For node n, cameFrom[n] is the node immediately preceding it on
+        # the cheapest path from start to n currently known.
         cameFrom = {} # an empty map
 
-        # For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+        # For node n, gScore[n] is the cost of the cheapest path from start
+        # to n currently known.
         Q = list(set(graph))
         gScore = {node: float('inf') for node in Q}
         gScore[start] = 0
@@ -38,17 +64,19 @@ def Functionality4(node, set_nodes, dist, nodes):
         fScore[start] = h(nodes, start, goal)
 
         while openSet:
-            current = min(openSet, key=fScore.get) # the node in openSet having the lowest fScore[] value
+            current = min(openSet, key=fScore.get) # the node in openSet having
+                                                   # the lowest fScore[] value
             if current == goal:
                 return reconstruct_path(cameFrom, current)
 
             openSet.remove(current)
             for neighbor in graph[current]:
-                # d(current,neighbor) is the weight of the edge from current to neighbor
-                # tentative_gScore is the distance from start to the neighbor through current
-                tentative_gScore = gScore[current] + graph[current][neighbor] # d(current, neighbor)
+                # d(current,neighbor) is the weight of the edge
+                # from current to neighbor
+                # tentative_gScore is the distance from start to the neighbor
+                # through current
+                tentative_gScore = gScore[current] + graph[current][neighbor]
                 if tentative_gScore < gScore[neighbor]:
-                    # This path to neighbor is better than any previous one. Record it!
                     cameFrom.update({neighbor: current})
                     gScore[neighbor] = tentative_gScore
                     fScore[neighbor] = gScore[neighbor] + h(nodes, neighbor, goal)
@@ -58,6 +86,11 @@ def Functionality4(node, set_nodes, dist, nodes):
         return 'Not possible'
 
     def sort_by_distance_as_the_crow_flies(node, set_nodes):
+
+        """
+        This function takes a list of nodes as input
+        and sorts them by distance as the crow flies
+        """
         node_to_sort = set_nodes[:-1]
         node_sorted = [node]
         while node_to_sort:
@@ -72,7 +105,13 @@ def Functionality4(node, set_nodes, dist, nodes):
         node_sorted += [set_nodes[-1]]
         return node_sorted
 
-    # Start functionality 4
+    """
+    + sorted set nodes
+    + calculate the path between two neighboring nodes
+    + print a path
+    + visualize a graph
+    + return a path
+    """
     sorted_set_nodes = sort_by_distance_as_the_crow_flies(node, set_nodes)
     path = [set_nodes[0]]
     for i in range(len(sorted_set_nodes)-2):
@@ -84,4 +123,5 @@ def Functionality4(node, set_nodes, dist, nodes):
         else:
             path += temp_path[1:]
     print(path)
+    ## TODO: visualize a graph  
     return path
