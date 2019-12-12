22cec78dce877b6cef234847344bae03f0bfb686
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-12-12 23:10:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy
 import gzip
-
 import func_1
 import func_2
 import func_3
@@ -12,91 +7,111 @@ import func_4
 
 def node_coordinate():
     """
-    :return: Pandas dataframe with node coordinates
-    EX:
-              Id Node    Latitude Longitude
-     0              1  -114315309  34133550
-     1              2  -114223946  34176221
-     ...          ...         ...       ...
+    {Id Node : {Latitude: int, longitude: int},....}
     """
-    Id_Node = []
-    Latitude = []
-    Longitude = []
+    nodes = {}
     with gzip.open('data/download/USA-road-d.CAL.co.gz', 'rt') as f:
         for line in f:
             if 'v' == line.split()[0]:
-                Id_Node.append(int(line.split()[1]))
-                Latitude.append(int(line.split()[2]))
-                Longitude.append(int(line.split()[3]))
-    dataframe = pd.DataFrame({'Id Node': Id_Node,
-                              'Latitude': Latitude,
-                              'Longitude': Longitude})
-    return dataframe
+                node = int(line.split()[1])
+                latitude = int(line.split()[2])
+                longitude = int(line.split()[3])
+                nodes[node] = {'Latitude': latitude, 'Longitude': longitude}
+    return nodes
 
 
 def node_distance():
     """
-    :return: Pandas dataframe with distances between each pair of nodes
-    EX:
-             Id Node 1 Id Node 2 Distance
-     0               1   1048577      456
-     1         1048577         1      456
-     ...          ...         ...       ...
+    {Id Node 1: {Id Node 2: weight, Id Node 2: weight},....}
     """
-    Id_Node1 = []
-    Id_Node2 = []
-    d = []
+    d = {}
     with gzip.open('data/download/USA-road-d.CAL.gr.gz', 'rt') as f:
         for line in f:
             if 'a' == line.split()[0]:
-                Id_Node1.append(int(line.split()[1]))
-                Id_Node2.append(int(line.split()[2]))
-                d.append(int(line.split()[3]))
-    dataframe = pd.DataFrame({'Id Node 1': Id_Node1,
-                              'Id Node 2': Id_Node2,
-                              'Weight': d})
-    return dataframe
+                node1 = int(line.split()[1])
+                node2 = int(line.split()[2])
+                w = int(line.split()[3])
+                if node1 in d:
+                    d[node1].update({node2: w})
+                else:
+                    d[node1] = {node2: w}
+    return d
 
 
 def node_travel_time():
     """
-    :return: Pandas dataframe with time distances between each pair of nodes
-    EX:
-               Id Node 1 Id Node 2 Distance
-       0               1   1048577      456
-       1         1048577         1      456
-     ...          ...         ...       ...
+    {Id Node 1: {Id Node 2: weight, Id Node 2: weight},....}
     """
-    Id_Node1 = []
-    Id_Node2 = []
-    t = []
+    t = {}
+    with gzip.open('data/download/USA-road-t.CAL.gr.gz', 'rt') as f:
+        for line in f:
+            if 'a' == line.split()[0]:
+                node1 = int(line.split()[1])
+                node2 = int(line.split()[2])
+                w = int(line.split()[3])
+                if node1 in t:
+                    t[node1].update({node2: w})
+                else:
+                    t[node1] = {node2: w}
+
+    return t
+
+
+def node_network_distance():
+    """
+    {Id Node 1: {Id Node 2: weight, Id Node 2: weight},....}
+    """
+    nd = {}
     with gzip.open('data/download/USA-road-d.CAL.gr.gz', 'rt') as f:
         for line in f:
             if 'a' == line.split()[0]:
-                Id_Node1.append(int(line.split()[1]))
-                Id_Node2.append(int(line.split()[2]))
-                t.append(int(line.split()[3]))
-    dataframe = pd.DataFrame({'Id Node 1': Id_Node1,
-                              'Id Node 2': Id_Node2,
-                              'Weight': t})
-    return dataframe
+                node1 = int(line.split()[1])
+                node2 = int(line.split()[2])
+                if node1 in nd:
+                    nd[node1].update({node2: 1})
+                else:
+                    nd[node1] = {node2: 1}
+    return nd
 
 
 def main():
     # Loading data
-    print("Loading data...")
-    node = node_coordinate()
+    print("Loading data...", end=' ')
+    nodes = node_coordinate()
     d = node_distance()
     t = node_travel_time()
-    net_d = d.copy()
-    net_d[['Weight']] = 1
-    # -------------
+    net_d = node_network_distance()
+    print("DONE!")
 
 
+    # Choice the function
+    print("\nChoose the function")
     n = int(input("Insert number (1-4): "))
     while not all([n > 0, n < 5]):
         n = int(input("Insert number (1-4): "))
 
+    # Choice the distance
+    print("\nChoose the distance")
+    dist = int(input("1) Distance\n" +
+                     "2) Travel time\n" +
+                     "3) Network distance\n" +
+                     "Insert number (1-3): "))
+    while not all([dist > 0, dist < 4]):
+        dist = int(input("1) Distance\n" +
+                         "2) Travel time\n" +
+                         "3) Network distance\n" +
+                         "Insert number (1-3): "))
+    if dist == 1:
+        dist = d
+    elif dist == 2:
+        dist = t
+    elif dist == 3:
+        dist = net_d
+
+    # Set start node
+    node = int(input("Start node: "))
+
+    # Switch
     if n == 1:
         # func_1
         pass
@@ -107,8 +122,9 @@ def main():
         # func_3
         pass
     elif n == 4:
-        # func_4
-        pass
+        print("Enter the nodes to be visited separated by white space:", end='')
+        set_nodes = list(map(int,input(" ").split()))
+        func_4.Functionality4(node, set_nodes, dist, nodes)
 
 
 if __name__:
