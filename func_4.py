5f71d0839f09fca1bb25b07aaa41f0dac28c645b
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-12-12 23:10:31 +0100

    Update func_4.py

--- func_4.py
+++ func_4.py
@@ -1,128 +1,87 @@
-import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy
-import gzip
 from math import sqrt
 
 
-def node_coordinate():
-    nodes = {}
-    with gzip.open('data/download/USA-road-d.CAL.co.gz', 'rt') as f:
-        for line in f:
-            if 'v' == line.split()[0]:
-                nodes[int(line.split()[1])] = {'Latitude': int(line.split()[2]),
-                                               'Longitude': int(line.split()[3])}
-    return nodes
+def Functionality4(node, set_nodes, dist, nodes):
+
+    def h(nodes, current, neighbor):
+        x1 = nodes[current]['Latitude']
+        y1 = nodes[current]['Longitude']
+        x2 = nodes[neighbor]['Latitude']
+        y2 = nodes[neighbor]['Longitude']
+        return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+    def reconstruct_path(cameFrom, current):
+        total_path = [current]
+        while current in cameFrom.keys():
+            current = cameFrom[current]
+            total_path.insert(0, current)
+        return total_path
 
 
-def node_distance():
-    d = {}
-    with gzip.open('data/download/USA-road-d.CAL.gr.gz', 'rt') as f:
-        for line in f:
-            if 'a' == line.split()[0]:
-                if int(line.split()[1]) in d:
-                    d[int(line.split()[1])].update({int(line.split()[2]): int(line.split()[3])})
-                    #d[int(line.split()[1])] = {k: v for k, v in sorted(d[int(line.split()[1])].items(), key=lambda x: x[1])}
-                else:
-                    d[int(line.split()[1])] = {int(line.split()[2]): int(line.split()[3])}
-                    #d[int(line.split()[1])] = {k: v for k, v in sorted(d[int(line.split()[1])].items(), key=lambda x: x[1])}
-    return d
+    # A* finds a path from start to goal.
+    # h is the heuristic function. h(n) estimates the cost to reach goal from node n.
+    def A_Star(nodes, graph, start, goal, h):
+        # The set of discovered nodes that may need to be (re-)expanded.
+        # Initially, only the start node is known.
+        openSet = {start}
 
+        # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
+        cameFrom = {} # an empty map
 
-def node_travel_time():
-    t = {}
-    with gzip.open('data/download/USA-road-t.CAL.gr.gz', 'rt') as f:
-        for line in f:
-            if 'a' == line.split()[0]:
-                if int(line.split()[1]) in t:
-                    t[int(line.split()[1])].update({int(line.split()[2]): int(line.split()[3])})
-                    #t[int(line.split()[1])] = {k: v for k, v in sorted(t[int(line.split()[1])].items(), key=lambda x: x[1])}
-                else:
-                    t[int(line.split()[1])] = {int(line.split()[2]): int(line.split()[3])}
-                    #t[int(line.split()[1])] = {k: v for k, v in sorted(t[int(line.split()[1])].items(), key=lambda x: x[1])}
+        # For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+        Q = list(set(graph))
+        gScore = {node: float('inf') for node in Q}
+        gScore[start] = 0
 
-    return t
+        # For node n, fScore[n] := gScore[n] + h(n).
+        fScore = {node: float('inf') for node in Q}
+        fScore[start] = h(nodes, start, goal)
 
+        while openSet:
+            current = min(openSet, key=fScore.get) # the node in openSet having the lowest fScore[] value
+            if current == goal:
+                return reconstruct_path(cameFrom, current)
 
-def node_network_distance():
-    """
-    {Id Node 1: {Id Node 2: weight, Id Node 2: weight},....}
-    """
-    nd = {}
-    with gzip.open('data/download/USA-road-d.CAL.gr.gz', 'rt') as f:
-        for line in f:
-            if 'a' == line.split()[0]:
-                if int(line.split()[1]) in nd:
-                    nd[int(line.split()[1])].update({int(line.split()[2]): 1})
-                else:
-                    nd[int(line.split()[1])] = {int(line.split()[2]): 1}
-    return nd
+            openSet.remove(current)
+            for neighbor in graph[current]:
+                # d(current,neighbor) is the weight of the edge from current to neighbor
+                # tentative_gScore is the distance from start to the neighbor through current
+                tentative_gScore = gScore[current] + graph[current][neighbor] # d(current, neighbor)
+                if tentative_gScore < gScore[neighbor]:
+                    # This path to neighbor is better than any previous one. Record it!
+                    cameFrom.update({neighbor: current})
+                    gScore[neighbor] = tentative_gScore
+                    fScore[neighbor] = gScore[neighbor] + h(nodes, neighbor, goal)
+                    if neighbor not in openSet:
+                        openSet.add(neighbor)
+        # Open set is empty but goal was never reached
+        return 'Not possible'
 
+    def sort_by_distance_as_the_crow_flies(node, set_nodes):
+        node_to_sort = set_nodes[:-1]
+        node_sorted = [node]
+        while node_to_sort:
+            d = float('inf')
+            for x in node_to_sort[:]:
+                d_temp = h(nodes, node_sorted[-1], x)
+                if d_temp <= d:
+                    d = d_temp
+                    next_node = x
+            node_sorted.append(next_node)
+            node_to_sort.remove(next_node)
+        node_sorted += [set_nodes[-1]]
+        return node_sorted
 
-print("Loading data...", end=' ')
-node = node_coordinate()
-d = node_distance()
-t = node_travel_time()
-net_d = node_network_distance()
-print("DONE!")
-
-
-def h(current, neighbor):
-    x1 = node[current]['Latitude']
-    y1 = node[current]['Longitude']
-    x2 = node[neighbor]['Latitude']
-    y2 = node[neighbor]['Longitude']
-    return sqrt(( x1 - x2)**2 + (y1 - y2)**2)
-
-
-def reconstruct_path(cameFrom, current):
-    total_path = [current]
-    while current in cameFrom.keys():
-        current = cameFrom[current]
-        total_path.insert(0, current)
-    return total_path
-
-
-# A* finds a path from start to goal.
-# h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-def A_Star(graph, start, goal, h):
-    # The set of discovered nodes that may need to be (re-)expanded.
-    # Initially, only the start node is known.
-    openSet = {start}
-
-    # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
-    cameFrom = {} # an empty map
-
-    # For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
-    Q = list(set(graph))
-    gScore = {node: float('inf') for node in Q}
-    gScore[start] = 0
-
-    # For node n, fScore[n] := gScore[n] + h(n).
-    fScore = {node: float('inf') for node in Q}
-    fScore[start] = h(start, goal)
-
-    while openSet:
-        current = min(openSet, key=fScore.get) # the node in openSet having the lowest fScore[] value
-        if current == goal:
-            return reconstruct_path(cameFrom, current)
-
-        openSet.remove(current)
-        for neighbor in graph[current]:
-            # d(current,neighbor) is the weight of the edge from current to neighbor
-            # tentative_gScore is the distance from start to the neighbor through current
-            tentative_gScore = gScore[current] + graph[current][neighbor] # d(current, neighbor)
-            if tentative_gScore < gScore[neighbor]:
-                # This path to neighbor is better than any previous one. Record it!
-                cameFrom.update({neighbor: current})
-                gScore[neighbor] = tentative_gScore
-                fScore[neighbor] = gScore[neighbor] + h(neighbor, goal)
-                if neighbor not in openSet:
-                    openSet.add(neighbor)
-
-    # Open set is empty but goal was never reached
-    return 'failure'
-
-
-print(A_Star(t, 1, 1048577, h))
+    # Start functionality 4
+    sorted_set_nodes = sort_by_distance_as_the_crow_flies(node, set_nodes)
+    path = [set_nodes[0]]
+    for i in range(len(sorted_set_nodes)-2):
+        start = sorted_set_nodes[i]
+        end = sorted_set_nodes[i+1]
+        temp_path = A_Star(nodes, dist, start, end, h)
+        if temp_path == 'Not possible':
+            return 'Not possible'
+        else:
+            path += temp_path[1:]
+    print(path)
+    return path
