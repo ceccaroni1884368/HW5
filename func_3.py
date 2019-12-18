af8ce0e77e29a9a01211323e43fc802e9f8bbcd3
clarapunzi <56367379+clarapunzi@users.noreply.github.com>
2019-12-18 16:42:26 +0100

    Update func_3.py

--- func_3.py
+++ func_3.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 import heapq 
 import gzip
-#
+import matplotlib.pyplot as plt
+import mplleaflet
+
 def dict_node_distance():
     """
     :return: Default dictionary having the nodes as keys and the list of connecetd nodes as values
@@ -66,13 +68,32 @@ def dijkstra(DICT, start, end):
                 heapq.heappush(queue, (new_weight, new_node, path))
     return (float('inf'), [])
 
+def node_coordinate():
+    """
+    :return: Pandas dataframe with node coordinates
+    EX:
+              Id Node    Latitude Longitude
+     0              1  -114315309  34133550
+     1              2  -114223946  34176221
+     ...          ...         ...       ...
+    """
+    Id_Node = []
+    Latitude = []
+    Longitude = []
+    with gzip.open('USA-road-d.CAL.co.gz', 'rt') as f:
+        for line in f:
+            if 'v' == line.split()[0]:
+                Id_Node.append(int(line.split()[1]))
+                Latitude.append(int(line.split()[2])/1000000)
+                Longitude.append(int(line.split()[3])/1000000)
+    dataframe = pd.DataFrame({'Id Node': Id_Node,'Latitude': Latitude,'Longitude': Longitude})
+    return dataframe
+
 D = dict_node_distance()
 T = dict_node_travel_time()
 N = dict_node_network_distance()
-
-
 dict_distances = {'d' : D, 't' : T, 'n' : N}  # d : physical distance, t : time distance, n : network distance
-
+nodesDF = node_coordinate()
 
 def Functionality3(node, list_nodes, distance):
     """ ARGUMENTS:
@@ -96,3 +117,25 @@ def Functionality3(node, list_nodes, distance):
         start = list_nodes[i]
     return (tot_dist, tot_path)
         
+def Visualization3(tot_path, list_nodes):
+    """ ARGUMENT:
+            tot_path : list of nodes composing the shortest path (list of int)
+            list_nodes : sequence of nodes that need to be visited in order (list of int)
+        RETURN:
+            visualize the shortest path on an interactive map (it opens a new tab in the browser)
+    """
+    shortest_path_coords = nodesDF[nodesDF['Id Node'].isin(tot_path)]
+    visit = nodesDF[nodesDF['Id Node'].isin(list_nodes)]
+    lats = shortest_path_coords.Longitude.values
+    longs = shortest_path_coords.Latitude.values
+    lats_visit = visit.Longitude.values
+    longs_visit = visit.Latitude.values
+    fig, ax = plt.subplots(figsize=(12,9))
+    ax.plot(longs, lats, 'orange', linewidth=1.5, marker =11, markersize=8)
+    ax.plot(longs[0], lats[0], 'gX', markersize=18)
+    ax.plot(longs_visit, lats_visit, 'bo', markersize = 10)
+    ax.plot(longs[-1], lats[-1], 'ro', markersize=18)
+    mplleaflet.show(fig=fig)
+#    mplleaflet.show(fig=fig, path='map.html')
+    return fig
+
