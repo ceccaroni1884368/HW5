12588fea6a4b0692c093aaf7b0dd4e3298fc400f
GamzaBakhishaliyeva <56519336+GamzaBakhishaliyeva@users.noreply.github.com>
2019-12-21 19:30:25 +0300

    Update func_1.py

--- func_1.py
+++ func_1.py
@@ -1,106 +1,122 @@
-import pdb
+import networkx as nx
+from collections import defaultdict
+import matplotlib.pyplot as plt 
+import pandas as pd
+import ipywidgets
 import gzip
-import numpy as np
+G = nx.Graph()
 
-
-def node_coordinate():
-    """
-    {Id Node : {Latitude: int, longitude: int},....}
-    """
-    nodes = {}
-    with gzip.open('USA-road-d.CAL.co.gz', 'rt') as f:
-        for line in f:
-            if 'v' == line.split()[0]:
-                node = int(line.split()[1])
-                latitude = int(line.split()[2])
-                longitude = int(line.split()[3])
-                nodes[node] = {'Latitude': latitude, 'Longitude': longitude}
-    return nodes
-
-
-def node_distance():
-    """
-    {Id Node 1: {Id Node 2: weight, Id Node 2: weight},....}
-    """
-    d = {}
-    with gzip.open('USA-road-d.CAL.gr.gz', 'rt') as f:
-        for line in f:
-            if 'a' == line.split()[0]:
-                node1 = int(line.split()[1])
-                node2 = int(line.split()[2])
-                w = int(line.split()[3])
-                if node1 in d:
-                    d[node1].update({node2: w})
+#graphnodes
+with gzip.open('data/download/USA-road-d.CAL.co', 'rt') as f1:
+    for line in f1:
+        if line[0] == 'v':
+            n,lo,la = list(map(int, line.strip().split()[1:]))
+            G.add_node(n,latitude = la/1000000,longitude = lo/1000000)
+            #edges
+            adj = defaultdict(set)
+with gzip.open('data/download/USA-road-d.CAL.gr', 'rt') as f:
+    for line in f:
+        if line[0] == 'a':
+            n1,n2, d =  list(map(int, line.strip().split()[1:]))
+            G.add_edge(n1,n2,distance = d,weight = 1)
+            adj[n1].add(n2)
+            adj[n2].add(n1)
+            #time
+            with gzip.open('data/download/USA-road-t.CAL.gr', 'rt') as f2:
+                for line in f2:
+                    if line[0] == 'a':
+                        n1,n2, t =  list(map(int, line.strip().split()[1:]))
+                        G.add_edge(n1,n2,time = t)
+            
+            #Functionalite1
+import heapq as hp
+def neighbors(v,w,d,graph = G, adjacent = adj):
+    visited = {v: 0} #take all weigths
+    F = [] #keep all nodes here
+    hp.heapify(F)
+    current_node = v #starting node
+    edges_min_d = set()
+    edges_max_d = set()
+    while True:
+        adjacents = adjacent[current_node]
+        weight_to_current_node = visited[current_node]
+        for node in adjacents:
+            weight = graph[current_node][node][w] + weight_to_current_node 
+            if weight > d: #filter, take only less than d
+                edges_max_d.add((current_node,node))
+            else:
+                edges_min_d.add((current_node,node)) #add visiting border
+                if node not in visited:
+                    visited[node] = weight
+                    hp.heappush(F,(weight,node))
                 else:
-                    d[node1] = {node2: w}
-    return d
+                    current_shortest_weight = visited[node]
+                    if current_shortest_weight > weight:
+                        visited[node] = weight
+                        hp.heappush(F,(weight,node))
+        try:
+            current_node = hp.heappop(F)[1]
+        except:
+            break
+    neighbors = list(visited.keys())
+    edges_max_d = list(edges_max_d)
+    edges_min_d = list(edges_min_d)
+    
+    maxres = edges_max_d
+    maxlst = []
+    for y in range (len(maxres)):
+        maxlst.append(maxres[y][0])
+        maxlst.append(maxres[y][1])
 
-def initialize(get_distance_graph, source):
-    d = {}
-    p = {}
-    for node in get_distance_graph:
-        d[node] = float('Inf')
-        p[node] = None
-    d[source] = 0
-    return d, p
+    notneighbors = list(set(maxlst) - set(neighbors))
+    
+    return neighbors,edges_min_d,edges_max_d,notneighbors
 
-def relax(node, neighbour, get_distance_graph, d, p):
+p = neighbors(11,'time',100000)
+print (p[0])
+print (p[1])
+print (p[2])
 
-    if d[neighbour] > d[node] + get_distance_graph[node][neighbour]:
+#visualization
+#map
+def map(nodelst): #start point
+    pos = G.nodes[nodelst[0]]
+    vismap = folium.Map(location=[pos['latitude'], pos['longitude']], zoom_start=10)
+    folium.raster_layers.TileLayer('Open Street Map').add_to(vismap)
+    folium.raster_layers.TileLayer('Stamen Terrain').add_to(vismap)
+    folium.LayerControl().add_to(vismap)
+    #minimap
+    visminimap = plugins.MiniMap(toggle_display=True)
+    #add to map
+    vismap.add_child(visminimap)
+    plugins.ScrollZoomToggler().add_to(vismap)
+    folium.Marker(location=[(pos['latitude']),(pos['longitude'])],
+                  icon=folium.Icon(color='red', icon='home'), popup = (nodelst[0])).add_to(vismap)
+    for i in range (len(nodelst)-1):
+        pos = (G.nodes[nodelst[i+1]])
+        folium.Marker(location=[(pos['latitude']),(pos['longitude'])],popup = (nodelst[i+1])).add_to(vismap)
+    
+    return vismap
+#adding all nodes to map
+def map_routes(lst,map_name):
+    for t in range (len(lst)):
+        cordlst = 0
+        cordlst = []
+        a = (lst[t])             
+        for i in a:
+            cordlst.append(list(G.nodes[i].values())) 
+        plugins.AntPath(cordlst).add_to(map_name)
+    return map_name
+    #function vizualization
+v = (int(input('Enter the node number: ')))
+p = (input('Enter the type of weight, either distance or time: '))
+d = int(input('Enter the thrushhold for the type of weight: '))
 
-        d[neighbour]  = d[node] + get_distance_graph[node][neighbour]
-        p[neighbour] = node
+nodelst = 0
+nodelst = neighbors(v,p,d,graph = G, adjacent = adj)
+omap = map(nodelst[0])
+map_routes(nodelst[1],omap)
+map_routespoly(nodelst[2],omap)
+circlemarker(nodelst[3],omap)
 
-def bellman_ford(get_distance_graph, source):
-    d, p = initialize(get_distance_graph, source)
-    for i in range(len(get_distance_graph)-1):
-        for u in get_distance_graph:
-            for v in get_distance_graph[u]:
-                relax(u, v, get_distance_graph, d, p)
-
-    for u in get_distance_graph:
-        for v in get_distance_graph[u]:
-            assert d[v] <= d[u] + get_distance_graph[u][v]
-
-    return d, p
-
-def functionality(strnode, vstnode):
-    vertices, edges = node_coordinate(), node_distance()
-
-    orderedn = find_order(strnode, vstnode, vertices)
-
-    best_path = bellman_ford(orderedn)
-
-    return best_path
-
-
-def find_order(start, vstnode, coordinates):
-    visit_list = list(vstnode)
-    ordered_path = []
-
-    while visit_list:
-
-        min_dist = real_dist(start, visit_list[0], coordinates)
-        nearest_node = visit_list[0]
-
-        for node in visit_list:
-            if min_dist > real_dist(start, node, coordinates):
-                min_dist = real_dist(start, node, coordinates)
-                nearest_node = node
-
-
-        visit_list.remove(nearest_node)
-        start = nearest_node
-        ordered_path.append(nearest_node)
-
-    return ordered_path
-
-def real_dist(node1, node2, coordinates):
-    return np.linalg.norm(np.array(coordinates[node1]) - np.array(coordinates[node2]))
-
-
-def bellman_ford(list_visit):
-    return list_visit
-
-
-#functionality(1, [5,4,7])
+omap.save('F1map.html')
